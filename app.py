9e1f39713b020356baa4f5dc395a8335859202e7
samrobertson-creator <110947628+samrobertson-creator@users.noreply.github.com>
2022-12-15 13:33:37 +1000

    defs

--- app.py
+++ app.py
@@ -1,21 +1,62 @@
-from Questions import Question
+def new_game():
+    
+    guesses = []
+    correct_guesses = 0
+    question_num = 1
 
-question_prompts = [
-    "How many hours are in a day?(a) 24(b) 25",
-    "How many hours are in a day?(a) 24(b) 25"
-]
+    for key in questions:
+        print("--------------------------------")
+        print(key)
+        for i in options[question_num:-1]:
+            print(i)
+        guess = input("Please enter (A, B, C, or D):")
+        guess = guess.upper()
+        guesses.append(guess)
 
-questions = [
-    Question(question_prompts[0], "a" ),
-    Question(question_prompts[1], "a" )
-]
+        correct_guesses += check_answer(questions.get(key), guess)
+        question_num  += 1
 
-def run_quiz(questions):
-    score = 0
-    for questions in questions:
-        answer = input(question_prompts)
-        if answer == questions.answer:
-            score += 1
-    print("You got " + str(score) + " / " + "2 " + "correct")
+    display_score(correct_guesses, guesses)
 
-run_quiz(questions)
+def check_answer(answer, guess):
+    if answer == guess:
+        print("Correct :)")
+        return 1
+    else:
+        print("Wrong :( ")
+        return 0
+
+def display_score(correct_guesses, guesses):
+    print("--------------------------------")
+    print("Results")
+    print("--------------------------------")
+    
+    print("Answers: ", end=" ")
+    for i in questions:
+        print(questions.get(i), end=" ")
+    print()
+
+    print("Guesses: ", end=" ")
+    for i in guesses:
+        print((i), end=" ")
+    print()
+
+    score = int((correct_guesses/len(questions))*100)
+    print("Your score is: " +str(score)+"%")
+
+def play_game():
+    pass
+
+questions = {
+"Who created Python?: ": "A",
+"Python is tributed to which comedy group?: ": "B"
+}
+
+options = [["A. Guido van Rossum", "B. Elon Musk", "C. Bill Gates", "D. Mark Zuckerberg"],
+          
+
+          ]
+
+def play_again
+
+new_game()
